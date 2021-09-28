a2734f5590a405736c4dea3c52a6b7c88e4390ca
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-28 12:31:52 +0530

    [ShubhamPV] TicTacToe

---
+++ TicTacToe/UC1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class UC1
+    {
+        public char[] Board()
+        {
+
+            char[] array = new char[10];
+            for (int i = 1; i <= array.Length; i++)
+            {
+                array[i] = ' ';
+            }
+            return array;
+        }
+        
+    }
+}
